f201ca3506fbdba2ba43417316d567a0ab64624f
zansangeeth <zansangeeth17@gmail.com>
2022-06-20 02:20:11 +0530

    added internet permissions, retrofit-gson dependencies and base URL for yelp API

--- app/src/main/java/com/zasa/simpleyelpapp/MainActivity.kt
+++ app/src/main/java/com/zasa/simpleyelpapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.zasa.simpleyelpapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+private const val BASE_URL = "https://api.yelp.com/v3/"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
