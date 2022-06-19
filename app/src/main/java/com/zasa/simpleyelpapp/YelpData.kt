85729a3711141fd958a9684393ffb3d248c4d00e
zansangeeth <zansangeeth17@gmail.com>
2022-06-20 03:13:15 +0530

    fully implemented expected JSON object for our recycler view

--- app/src/main/java/com/zasa/simpleyelpapp/YelpData.kt
+++ app/src/main/java/com/zasa/simpleyelpapp/YelpData.kt
@@ -18,6 +18,21 @@ data class YelpRestaurant(
     val price : String,
     @SerializedName("review_count") val numReviews : Int,
     @SerializedName("distance") val distanceInMeters : Double,
-    @SerializedName("image_url") val imageUrl : String
+    @SerializedName("image_url") val imageUrl : String,
+    val category : List<YelpCategory>,
+    val yelpLocation: YelpLocation
+){
+    fun displayDistance() : String{
+        val milesPerMeter = 0.000621371
+        val distanceInMiles = "%.2f".format(distanceInMeters * milesPerMeter)
+        return "$distanceInMiles mi"
+    }
+}
 
+data class YelpCategory(
+    val title : String
+)
+
+data class YelpLocation(
+    @SerializedName("address1") val address : String
 )
